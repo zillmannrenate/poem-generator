3908a1688d2d2e89bfb198c599a687fd0ec115da
zillmannrenate <138320454+zillmannrenate@users.noreply.github.com>
2024-01-23 01:00:01 +0100

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function writePoem(response) {
   let poem = response.data.answer;
-  console.log(poem);
+
   new Typewriter("#verses", {
     strings: poem,
     autoStart: true,
@@ -17,9 +17,12 @@ function generatePoem(event) {
   let apiKey = "407bfao61470cbta2ae38d4403fe23ad";
   let context =
     "you are very creative, funny and a romantic poem expert and love to write profound poems. please be creative and behave. your mission is to generate a 4 line poem in basic HTML. make sure to follow the user instructions";
-  let prompt = `user instructions: Please be so kind and compose a 4 line german poem about ${poemTopic}. Display lines in separate <br/>. No title. At the end sign off with"SheCodes AI" `;
+  let prompt = `user instructions: Please be so kind and compose a 4 line german poem about ${poemTopic}. Display lines in separate <br/>. No title. At the end sign the poem in a fifth and different line with "SheCodes AI" inside a <strong> element`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#verses");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛Generating german poem about ${poemTopic}...</div>`;
   axios.get(apiUrl).then(writePoem);
 }
 
